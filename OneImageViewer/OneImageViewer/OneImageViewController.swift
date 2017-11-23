07642cc2486a1ae703964a14f7b06c627e2708db
littleweh <adakao@gmail.com>
2017-11-23 14:45:37 +0800

    setUp bottomView and pickAnImageButton layout

--- OneImageViewer/OneImageViewer/OneImageViewController.swift
+++ OneImageViewer/OneImageViewer/OneImageViewController.swift
@@ -16,13 +16,31 @@ class OneImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // ToDo: initial UIScrollView with frame
-//        scrollView = UIScrollView()
-//        imageView = UIImageView(image: UIImage(named: "icon_photo"))
-        
-        
-        bottomView = UIView()
+        scrollView = UIScrollView()
+        imageView = UIImageView(image: UIImage(named: "icon_photo"))
         let rect = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 77)
+        scrollView.frame = rect
+        self.view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        
+        
+        
+        
+        
+        
+        
+//        setUpScrollView()
+//        setUpImageView()
+        setUpBottomView()
+        setUpPickAnImageButton()
+    } // viewDidLoad
+    
+    func setUpBottomView() {
+        let margins = self.view.layoutMarginsGuide
+        let rect = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 77)
+        bottomView = UIView()
         bottomView.frame = rect
         bottomView.backgroundColor = UIColor(
             red: 249.0/255.0,
@@ -31,7 +49,19 @@ class OneImageViewController: UIViewController {
             alpha: 1.0
         )
         self.view.addSubview(bottomView)
+        bottomView.translatesAutoresizingMaskIntoConstraints = false
         
+        NSLayoutConstraint.activate([
+            bottomView.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: 0),
+            bottomView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 0),
+            bottomView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: 0),
+            bottomView.heightAnchor.constraint(equalToConstant: 77),
+            bottomView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+        
+    } //setUpBottomView
+
+    func setUpPickAnImageButton() {
         pickAnImageButton = UIButton()
         pickAnImageButton.frame = CGRect(x: 0, y: 0, width: 180, height: 44)
         pickAnImageButton.backgroundColor = UIColor(
@@ -40,26 +70,31 @@ class OneImageViewController: UIViewController {
             blue: 43.0/255.0,
             alpha: 1.0
         )
+
         pickAnImageButton.setTitle("Pick an Image", for: .normal)
         pickAnImageButton.setTitleColor(.white, for: .normal)
         pickAnImageButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
-        pickAnImageButton.contentHorizontalAlignment = .left
-        self.bottomView.addSubview(pickAnImageButton)
-        
-        
-        
-//        setUpScrollView()
-//        setUpImageView()
-//        setUpBottomView()
-//        setUpPickAnImageButton()
-    } // viewDidLoad
-    
-    func setUpBottomView() {
-        
-        
-    } //setUpBottomView
+        pickAnImageButton.contentHorizontalAlignment = .center
 
-    func setUpPickAnImageButton() {
+        pickAnImageButton.layer.cornerRadius = 2
+        pickAnImageButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+        pickAnImageButton.layer.shadowColor = UIColor(
+            red: 0/255.0,
+            green: 0/255.0,
+            blue: 0/255.0,
+            alpha: 0.25
+        ).cgColor
+        pickAnImageButton.layer.masksToBounds = true
+        
+        self.bottomView.addSubview(pickAnImageButton)
+        pickAnImageButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            pickAnImageButton.centerYAnchor.constraint(equalTo: bottomView.centerYAnchor),
+            pickAnImageButton.centerXAnchor.constraint(equalTo: bottomView.centerXAnchor),
+            pickAnImageButton.heightAnchor.constraint(equalToConstant: bottomView.frame.size.height * 4 / 7 ),
+            pickAnImageButton.widthAnchor.constraint(equalToConstant: bottomView.frame.size.width / 2)
+        ])
     } //setUpPickAnImageButton
 
     func setUpScrollView() {
