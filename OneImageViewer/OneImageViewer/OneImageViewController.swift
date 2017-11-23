f6eb072792109cc9321e3746f3955765965f88ec
littleweh <adakao@gmail.com>
2017-11-23 14:22:51 +0800

    try to add bottomView and button programmatically

--- OneImageViewer/OneImageViewer/OneImageViewController.swift
+++ OneImageViewer/OneImageViewer/OneImageViewController.swift
@@ -9,27 +9,67 @@
 import UIKit
 
 class OneImageViewController: UIViewController {
-
+    var scrollView: UIScrollView!
+    var imageView: UIImageView!
+    var bottomView: UIView!
+    var pickAnImageButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // ToDo: initial UIScrollView with frame
+//        scrollView = UIScrollView()
+//        imageView = UIImageView(image: UIImage(named: "icon_photo"))
+        
+        
+        bottomView = UIView()
+        let rect = CGRect(x: 0, y: 0, width: self.view.bounds.width, height: 77)
+        bottomView.frame = rect
+        bottomView.backgroundColor = UIColor(
+            red: 249.0/255.0,
+            green: 223.0/255.0,
+            blue: 23.0/255.0,
+            alpha: 1.0
+        )
+        self.view.addSubview(bottomView)
+        
+        pickAnImageButton = UIButton()
+        pickAnImageButton.frame = CGRect(x: 0, y: 0, width: 180, height: 44)
+        pickAnImageButton.backgroundColor = UIColor(
+            red: 43.0/255.0,
+            green: 43.0/255.0,
+            blue: 43.0/255.0,
+            alpha: 1.0
+        )
+        pickAnImageButton.setTitle("Pick an Image", for: .normal)
+        pickAnImageButton.setTitleColor(.white, for: .normal)
+        pickAnImageButton.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
+        pickAnImageButton.contentHorizontalAlignment = .left
+        self.bottomView.addSubview(pickAnImageButton)
+        
+        
+        
+//        setUpScrollView()
+//        setUpImageView()
+//        setUpBottomView()
+//        setUpPickAnImageButton()
+    } // viewDidLoad
+    
+    func setUpBottomView() {
+        
+        
+    } //setUpBottomView
 
-        // Do any additional setup after loading the view.
-    }
+    func setUpPickAnImageButton() {
+    } //setUpPickAnImageButton
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    func setUpScrollView() {
+    } //setUpScrollView
+
+
+    func setUpImageView() {
+    } //setUpImageView
+    
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+} // class OneImageViewController
